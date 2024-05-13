0eaeef21aa47dc5f9b79c444db49fd0dbb71eda6
Glooring <dan_avramescu_12@yahoo.com>
2024-05-14 00:44:23 +0300

    Updated server.js to use OAuth 2.0 with passport-twitter

--- server.js
+++ server.js
@@ -6,43 +6,43 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Twitter API credentials
-const consumerKey = process.env.CONSUMER_KEY || 'v71tqJheXMkovRUcb5UGfylps';
-const consumerSecret = process.env.CONSUMER_SECRET || 'uT6fY1GKYlQbmyAK2QpTGzvzVMnWnAKSdqfcubeG6cwh0cfyfa';
+const consumerKey = 'v71tqJheXMkovRUcb5UGfylps';
+const consumerSecret = 'uT6fY1GKYlQbmyAK2QpTGzvzVMnWnAKSdqfcubeG6cwh0cfyfa';
 const callbackURL = 'https://twitter-auth-app.vercel.app/callback';
 
 // Initialize OAuth
 const twitterOAuth = new OAuth(
-  'https://api.twitter.com/oauth/request_token',
-  'https://api.twitter.com/oauth/access_token',
-  consumerKey,
-  consumerSecret,
-  '1.0A',
-  callbackURL,
-  'HMAC-SHA1'
+    'https://api.twitter.com/oauth/request_token',
+    'https://api.twitter.com/oauth/access_token',
+    consumerKey,
+    consumerSecret,
+    '1.0A',
+    callbackURL,
+    'HMAC-SHA1'
 );
 
 // Twitter authentication route
 app.get('/auth/twitter', (req, res) => {
-  twitterOAuth.getOAuthRequestToken((error, oauthToken, oauthTokenSecret, results) => {
-    if (error) {
-      res.send('Authentication failed!');
-    } else {
-      res.redirect(`https://twitter.com/oauth/authenticate?oauth_token=${oauthToken}`);
-    }
-  });
+    twitterOAuth.getOAuthRequestToken((error, oauthToken, oauthTokenSecret, results) => {
+        if (error) {
+            res.send('Authentication failed!');
+        } else {
+            res.redirect(`https://twitter.com/oauth/authenticate?oauth_token=${oauthToken}`);
+        }
+    });
 });
 
 // Callback URL route
 app.get('/callback', (req, res) => {
-  const oauthToken = req.query.oauth_token;
-  const oauthVerifier = req.query.oauth_verifier;
-  twitterOAuth.getOAuthAccessToken(oauthToken, null, oauthVerifier, (error, oauthAccessToken, oauthAccessTokenSecret, results) => {
-    if (error) {
-      res.send('Authentication failed!');
-    } else {
-      res.send('Authentication successful!');
-    }
-  });
+    const oauthToken = req.query.oauth_token;
+    const oauthVerifier = req.query.oauth_verifier;
+    twitterOAuth.getOAuthAccessToken(oauthToken, null, oauthVerifier, (error, oauthAccessToken, oauthAccessTokenSecret, results) => {
+        if (error) {
+            res.send('Authentication failed!');
+        } else {
+            res.send('Authentication successful!');
+        }
+    });
 });
 
 // Serve terms of service and privacy policy
@@ -51,10 +51,10 @@ app.use('/privacy', express.static(path.join(__dirname, 'public/privacy.html')))
 
 // Default route
 app.get('/', (req, res) => {
-  res.send('Welcome to the Twitter OAuth App');
+    res.send('Welcome to the Twitter OAuth App');
 });
 
 // Start server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
